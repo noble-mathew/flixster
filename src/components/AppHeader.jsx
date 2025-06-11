6754a76cd9a1b58185ac2bee372b7dcbb35477a6
Noble Tom Mathew <nobletm@meta.com>
2025-06-11 16:31:47 -0700

    fixed header image not loading

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,7 @@ import "../App.css"
 function AppHeader({ onClick }) {
     return (
         <header>
-            <img src="../../public/movie.png"/>
+            <img src="movie.png"/>
             <h1 onClick={onClick}>Flixster</h1>
         </header>
     )
